cc99dd8a1da15750d4af130646c3082ddd3a13cd
omerbd21 <omerbendavid21@gmail.com>
2020-12-15 12:45:23 +0200

    deleted view programs and added ssh via hostkey only

--- pkg/linux/ssh.go
+++ pkg/linux/ssh.go
@@ -1,5 +1,92 @@
-package linux
+package main
 
-func Ssh(){
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
+func getHostKey(host string) (ssh.PublicKey, error) {
+	file, err := os.Open(filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts"))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var hostKey ssh.PublicKey
+	for scanner.Scan() {
+		fields := strings.Split(scanner.Text(), " ")
+		if len(fields) != 3 {
+			continue
+		}
+		if strings.Contains(fields[0], host) {
+			var err error
+			hostKey, _, _, _, err = ssh.ParseAuthorizedKey(scanner.Bytes())
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("error parsing %q: %v", fields[2], err))
+			}
+			break
+		}
+	}
+
+	if hostKey == nil {
+		return nil, errors.New(fmt.Sprintf("no hostkey for %s", host))
+	}
+	return hostKey, nil
+}
+func runCommand(ip string, command string ) string {
+	hostKey, err := getHostKey(ip)
+	key, err := ioutil.ReadFile(os.Getenv("HOME")+"\\.ssh\\id_rsa")
+	if err != nil {
+		log.Fatalf("unable to read private key: %v", err)
+	}
+
+	// Create the Signer for this private key.
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatalf("unable to parse private key: %v", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{
+			// Use the PublicKeys method for remote authentication.
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.FixedHostKey(hostKey),
+	}
+	// Connect to ssh server
+	conn, err := ssh.Dial("tcp", ip+":22", config)
+	if err != nil {
+		log.Fatal("unable to connect: ", err)
+	}
+	defer conn.Close()
+	// Create a session
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Fatal("unable to create session: ", err)
+	}
+	defer session.Close()
+	var output bytes.Buffer
+	session.Stdout = os.Stdout
+	session.Stdout = &output
+	session.Stdin = os.Stdin
+	err = session.Run(command)
+	if err != nil{
+		log.Fatal(err)
+	}
+	return output.String()
+
+
+}
+func main() {
+	fmt.Println(runCommand("192.168.43.126", "cat /root/.ssh/known_hosts"))
 }
